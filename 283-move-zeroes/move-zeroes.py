c952a4b49b52c5977a02b2e3614af944d6a10284
Dave <121303027+dawit-shewangzaw@users.noreply.github.com>
2026-03-12 14:27:42 +0300

    Time: 9 ms (21.74%) | Memory: 20.5 MB (56.90%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,11 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        arr_length = len(nums)
         holder = 0 
         seeker = 0
 
-        while seeker < len(nums):
+        while seeker < arr_length:
             if nums[seeker] != 0:
                 nums[seeker] , nums[holder] = nums[holder] , nums[seeker]
                 holder += 1
             seeker += 1
+        return nums
