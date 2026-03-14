eb9ea7c55ffa27887aff943db08d3162e5b5c219
Dave <121303027+dawit-shewangzaw@users.noreply.github.com>
2026-03-14 11:58:20 +0300

    Time: 81 ms (27.73%) | Memory: 29.1 MB (63.24%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,14 +1,94 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        # First we make the window result
-        total = sum(nums[0:k]) 
-        output = total 
+        
+        window_sum = float('-inf')
+        left , right = 0 , k
+        first_sum = 0
 
-        for i in range(len(nums)-k):
-            # Add the right next num to total
-            total += nums[i+k]
-            # Substruct the most left nums from the window 
-            total -= nums[i]
-            # Compare from the past total and assign the maximum to the output  
-            output = max(total ,output)
-        return output/k
+        for i in range(k):
+            first_sum += nums[i]
+            
+        window_sum = max(first_sum , window_sum)
+
+        while right < len(nums):
+            first_sum += nums[right]
+            first_sum -= nums[left]
+        
+            window_sum = max(window_sum , first_sum)
+            left += 1
+            right += 1
+        
+        return window_sum / k
+
+
+        # for i in range(k):
+        #     first_sum += nums[i]
+            
+        #     window_sum = max(first_sum , window_sum)
+        
+        # for right in range(k , len(nums)):
+        #     first_sum += nums[right]
+        #     first_sum -= nums[left]
+        
+        #     window_sum = max(window_sum , first_sum)
+        #     left += 1
+        
+        # return window_sum / k
+        
+        # window_sum = float('-inf')
+        # left = 0
+        # first_sum = 0
+
+        # for i in range(k):
+        #     first_sum += nums[i]
+            
+        #     window_sum = max(first_sum , window_sum)
+        
+        # for right in range(k , len(nums)):
+        #     first_sum += nums[right]
+        #     first_sum -= nums[left]
+        
+        #     window_sum = max(window_sum , first_sum)
+        #     left += 1
+        
+        # return window_sum / k
+
+
+
+
+
+
+
+
+
+
+# # class Solution:
+# #     def findMaxAverage(self, nums: List[int], k: int) -> float:
+# #         # First we make the window result
+# #         window_sum = sum(nums[0:k]) 
+# #         output = window_sum 
+
+# #         for i in range(len(nums)-k):
+# #             # Add the right next num to window_sum
+# #             window_sum += nums[i+k]
+# #             # Substruct the most left nums from the window 
+# #             window_sum -= nums[i]
+# #             # Compare from the past window_sum and assign the maximum to the output  
+# #             output = max(window_sum ,output)
+# #         return output/k
+
+# class Solution:
+#     def findMaxAverage(self, nums: List[int], k: int) -> float:
+        
+#         window_sum = sum(nums[:k])
+#         max_sum = window_sum
+#         left = 0
+
+#         for right in range(k,len(nums)):
+#             window_sum += nums[right]
+#             window_sum -= nums[left]
+#             left += 1
+
+#             max_sum = max(max_sum, window_sum)
+
+#         return max_sum / k
