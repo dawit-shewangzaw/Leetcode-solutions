8a9dc7e32f42312676d6c53eafeefcec2f7f814b
Dave <121303027+dawit-shewangzaw@users.noreply.github.com>
2026-03-17 19:03:41 +0300

    Time: 3 ms (74.59%) | Memory: 19.3 MB (50.59%) - LeetSync

--- 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.py
+++ 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.py
@@ -1,91 +1,27 @@
 class Solution:
     def countVowelSubstrings(self, word: str) -> int:
-        def atMost(k):
-            vowels = set('aeiou')
-            counts = {}
-            left = 0
-            ans = 0
-            for right in range(len(word)):
-                
-                if word[right] not in vowels:
-                    counts = {}
-                    left = right + 1
-                    continue
-                
-                counts[word[right]] = counts.get(word[right], 0) + 1
-                
-                while len(counts) > k:
-                    counts[word[left]] -= 1
-                    if counts[word[left]] == 0:
-                        del counts[word[left]]
-                    left += 1
-                
-                ans += (right - left + 1)
-            return ans
-
-        return atMost(5) - atMost(4)
-
-# class Solution:
-#     def countVowelSubstrings(self, word: str) -> int:
-
-#         vowel = "aeiou"
-#         count = 0
-#         output = 0
-#         left = 0
-
-#         for i in range(5):
-#             if word[i] in vowel:
-#                 count += 1
-#                 print("i " + str(word[i]))
-#                 print("count for" + str(count))
-#             if count == 5:
-#                 output += 1
-#                 print("output first " + str(output))
-                
-
-#         for right in range(5 , len(word)):
-#             print("right " + str(word[right]))
-            
-#             if (word[right]) in vowel:
-#                 count += 1
-#                 print("count if right" + str(count))
-            
-#             if (word[left]) in vowel:
-#                 count -= 1
-#                 print("count if left " + str(count))
-
-#             if count == 5:
-#                 output += 1
-#                 print("output last " + str(output))
-#             left += 1
         
-#         return output
-        
-        # vowel = "aeiou"
-        # output = 0
-        # max_no = float("-inf")
-        # left , right = 0 , k
-        # three = ''
+        vowels = set("aeiou")
+        total = 0
+        left = 0
+        mid = 0
+        count = {}
 
-        # for _ in range(k):
-        #     print(s[_])
-        #     three += s[_]
-        #     if s[_] in vowel:
-        #         output += 1
-        #         print(output)
-        # max_no = max(max_no , output)
-        
-        # while right < len(s):
-            
-        #     if (s[right]) in vowel:
-        #         output += 1
-            
-        #     if (s[left]) in vowel:
-        #         output -= 1
+        for right in range(len(word)):
+            # 1. If we hit a consonant, reset everything
+            if word[right] not in vowels:
+                count.clear()
+                left = mid = right + 1
+                continue
 
-        #     right += 1
-        #     left += 1
-        #     max_no = max(max_no , output)
-        # max_no = max(max_no , output)
-        
-        # return max_no
+            count[word[right]] = count.get(word[right], 0) + 1
+
+            while len(count) == 5:
+                count[word[mid]] -= 1
+                if count[word[mid]] == 0:
+                    del count[word[mid]]
+                mid += 1
+            
+            total += (mid - left)
+
+        return total   
