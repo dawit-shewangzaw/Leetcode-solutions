cb264d5e921423da8f9d79f33672ef4e65773aab
Dave <121303027+dawit-shewangzaw@users.noreply.github.com>
2026-03-23 12:12:23 +0300

    Time: 139 ms (33.64%) | Memory: 37 MB (38.71%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.py
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.py
@@ -1,6 +1,5 @@
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
-        from collections import defaultdict
         
         freq = defaultdict(int)
         prev_count = 0
