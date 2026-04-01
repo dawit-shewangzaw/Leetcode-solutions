8ade7c27cd55e513a02bcdb07648a686b5bcd63b
Dave <121303027+dawit-shewangzaw@users.noreply.github.com>
2026-04-01 10:48:47 +0300

    Time: 335 ms (20.04%) | Memory: 19.8 MB (12.46%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,17 +1,38 @@
-# Selection sort
+# Insertion sort
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        n = len(heights)    
-        
-        for i in range(n):
-            max_index = i
-            for j in range(i + 1, n):
-                
-                if heights[j] >= heights[max_index]:
-                    max_index = j
-            heights[i] , heights[max_index] = heights[max_index] , heights[i]
-            names[i] , names[max_index] = names[max_index] , names[i]
+        n = len(heights)
+
+        for i in range(1, n):
+            key_height = heights[i]
+            key_name = names[i]
+            
+            j = i - 1
+            
+            while j >= 0 and key_height > heights[j]:
+                heights[j + 1] = heights[j]
+                names[j + 1] = names[j]
+                j -= 1
+            
+            heights[j + 1] = key_height
+            names[j + 1] = key_name
+            
         return names
+
+# Selection sort
+# class Solution:
+#     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+#         n = len(heights)    
+        
+#         for i in range(n):
+#             max_index = i
+#             for j in range(i + 1, n):
+                
+#                 if heights[j] >= heights[max_index]:
+#                     max_index = j
+#             heights[i] , heights[max_index] = heights[max_index] , heights[i]
+#             names[i] , names[max_index] = names[max_index] , names[i]
+#         return names
         
 # Bubble sort
 # class Solution:
